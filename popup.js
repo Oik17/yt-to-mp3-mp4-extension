a0fdb17234a61d41691224d309b5dfcb13dec856
Akshat Gupta <akshat.g1707@gmail.com>
2025-04-08 11:08:53 +0530

    fix js

--- popup.js
+++ popup.js
@@ -1,29 +1,31 @@
+document.getElementById("mp3Btn").addEventListener("click", () => download("mp3"));
+document.getElementById("mp4Btn").addEventListener("click", () => download("mp4"));
+
 function download(format) {
-    const url = document.getElementById("url").value;
-    const status = document.getElementById("status");
-  
-    if (!url.startsWith("http")) {
-      status.textContent = "Please enter a valid URL";
-      return;
-    }
-  
-    const apiURL = `http://localhost:8080/download?url=${encodeURIComponent(url)}&format=${format}`;
-    status.textContent = "Downloading...";
-  
-    fetch(apiURL)
-      .then(res => {
-        if (!res.ok) throw new Error("Download failed");
-        return res.blob();
-      })
-      .then(blob => {
-        const a = document.createElement("a");
-        a.href = URL.createObjectURL(blob);
-        a.download = `video.${format}`;
-        a.click();
-        status.textContent = "Download started!";
-      })
-      .catch(err => {
-        status.textContent = "Failed: " + err.message;
-      });
+  const url = document.getElementById("url").value;
+  const status = document.getElementById("status");
+
+  if (!url.startsWith("http")) {
+    status.textContent = "Please enter a valid URL";
+    return;
   }
-  
+
+  const apiURL = `https://caterpillar-hack-production.up.railway.app/download?url=${encodeURIComponent(url)}&format=${format}`;
+  status.textContent = "Downloading...";
+
+  fetch(apiURL)
+    .then(res => {
+      if (!res.ok) throw new Error("Download failed");
+      return res.blob();
+    })
+    .then(blob => {
+      const a = document.createElement("a");
+      a.href = URL.createObjectURL(blob);
+      a.download = `video.${format}`;
+      a.click();
+      status.textContent = "Download started!";
+    })
+    .catch(err => {
+      status.textContent = "Failed: " + err.message;
+    });
+}
